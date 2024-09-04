31c7c7bfc08a9930c036b9a3fdd612f6ec4319e5
amarek <alexander.marek@outlook.com>
2024-09-04 11:15:00 +0200

    added huge testfile

--- TilingSample.Tests/TileGeneratorTests.cs
+++ TilingSample.Tests/TileGeneratorTests.cs
@@ -8,19 +8,36 @@ namespace TilingSample.Tests
 {
     public class TileGeneratorTests
     {
+        const string smallFileName = "mountain_4000x1800";
+        private const string largeFileName = "landscape_12000x6000";
+
         [Fact]
-        public void CanCarateTiles()
+        public void CanCreateTiles()
         {
-            var file = Path.Combine(Environment.CurrentDirectory, "high-resolution-mountains-and-lakes-vast-xl.jpg");
-            var tileDir = Path.Combine(Environment.CurrentDirectory, "tiles");
+            var file = Path.Combine(Environment.CurrentDirectory, $"{smallFileName}.jpg");
+            var tileDir = Path.Combine(Environment.CurrentDirectory, $"tiles_{smallFileName}");
 
             var gen = new TileGenerator();
 
             gen.GenerateTiles(file, tileDir);
 
-            Assert.True(Directory.EnumerateFiles(tileDir).Any());
+            var tiles = Directory.EnumerateFiles(tileDir, "*.*", SearchOption.AllDirectories);
+            Assert.True(tiles.Any());
         }
 
+        [Fact]
+        public void CanCreateTiles_Huge()
+        {
+            var file = Path.Combine(Environment.CurrentDirectory, $"{largeFileName}.jpg");
+            var tileDir = Path.Combine(Environment.CurrentDirectory, $"tiles_{largeFileName}");
+
+            var gen = new TileGenerator();
+
+            gen.GenerateTiles(file, tileDir);
+
+            var tiles = Directory.EnumerateFiles(tileDir, "*.*", SearchOption.AllDirectories);
+            Assert.True(tiles.Any());
+        }
 
         [Theory]
         [InlineData(1.0f)]
@@ -29,7 +46,8 @@ namespace TilingSample.Tests
         [InlineData(0.125f)]
         public void CanRenderAtOriginWithDifferentZoomFactors(float zoomFactor)
         {
-            var tileDir = Path.Combine(Environment.CurrentDirectory, "tiles");
+            var fileName = smallFileName;
+            var tileDir = Path.Combine(Environment.CurrentDirectory, $"tiles_{fileName}");
             var rndr = new TileRenderer(800, 600);
             var fn = Path.Combine(Environment.CurrentDirectory, $"x0_y0-z{(1f/zoomFactor).ToString(CultureInfo.InvariantCulture)}-rnder{rndr.Width}x{rndr.Height}.jpeg");
 
@@ -43,7 +61,8 @@ namespace TilingSample.Tests
         [InlineData(0.25f)]
         public void CanRenderAtImageCenterWithDifferentZoomFactors(float zoomFactor)
         {
-            var tileDir = Path.Combine(Environment.CurrentDirectory, "tiles");
+            var fileName = smallFileName;
+            var tileDir = Path.Combine(Environment.CurrentDirectory, $"tiles_{fileName}");
             var rndr = new TileRenderer(800, 600);
             var fn = Path.Combine(Environment.CurrentDirectory, $"x1700_y900z-{(1f / zoomFactor).ToString(CultureInfo.InvariantCulture)}-rnder{rndr.Width}x{rndr.Height}.jpeg");
 
@@ -54,6 +73,25 @@ namespace TilingSample.Tests
             var y = cy;// * zoomFactor;
 
             rndr.RenderBitmap(tileDir, fn, x, y, zoomFactor);
+        }
+
+
+        [Theory]
+        [InlineData(1.0f)]
+        [InlineData(0.5f)]
+        [InlineData(0.25f)]
+        [InlineData(0.125f)]
+        [InlineData(0.0625f)]
+        [InlineData(0.03125f)]
+        public void CanRenderHugeImageAtOriginWithDifferentZoomFactors(float zoomFactor)
+        {
+            var fileName = largeFileName;
+            var tileDir = Path.Combine(Environment.CurrentDirectory, $"tiles_{fileName}");
+            var rndr = new TileRenderer(800, 600);
+            var fn = Path.Combine(Environment.CurrentDirectory, $"{fileName}x0_y0-z{(1f / zoomFactor).ToString(CultureInfo.InvariantCulture)}-rnder{rndr.Width}x{rndr.Height}.jpeg");
+
+
+            rndr.RenderBitmap(tileDir, fn, 0, 0, zoomFactor);
         }
     }
 }
